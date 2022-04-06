1afe5d133976f325d5fa48c11082e25b0ee1af7a
jthakur02 <jthakur@uncc.edu>
2022-04-06 03:30:13 -0400

    Added 1.9 and 2.1

--- discrete.js
+++ discrete.js
@@ -26,6 +26,7 @@ window.onload = function()
     _("d1_6x1").onclick = d1_6x1;
     _("d1_7x1").onclick = d1_7x1;
     _("d1_8x1").onclick = d1_8x1;
+    _("d1_9x1").onclick = d1_9x1;
 
     /* discrete interactive */
 
@@ -358,6 +359,93 @@ window.onload = function()
         if (d1_8x1_bool[3]) {document.getElementById("d1_8x1_r4").style.backgroundColor = "#739E5B"; document.getElementById("d1_8x1_r4").innerHTML = "correct";}
     }
     // 1.9
+    // Activity 1
+    function d1_9x1() {
+        document.getElementById("d1_9x1_result").style.display = "block";
+
+        var d1_9x1_a = document.getElementById("d1_9x1a").checked;
+        var d1_9x1_b = document.getElementById("d1_9x1b").checked;
+        var d1_9x1_c = document.getElementById("d1_9x1c").checked;
+        var d1_9x1_d = document.getElementById("d1_9x1d").checked;
+        var d1_9x1_e = document.getElementById("d1_9x1e").checked;
+        var d1_9x1_f = document.getElementById("d1_9x1f").checked;
+        var d1_9x1_g = document.getElementById("d1_9x1g").checked;
+        var d1_9x1_h = document.getElementById("d1_9x1h").checked;
+        var d1_9x1_i = document.getElementById("d1_9x1i").checked;
+        var d1_9x1_j = document.getElementById("d1_9x1j").checked;
+        var d1_9x1_k = document.getElementById("d1_9x1k").checked;
+        var d1_9x1_l = document.getElementById("d1_9x1l").checked;
+        var d1_9x1_m = document.getElementById("d1_9x1m").checked;
+        var d1_9x1_n = document.getElementById("d1_9x1n").checked;
+        var d1_9x1_o = document.getElementById("d1_9x1o").checked;
+        var d1_9x1_p = document.getElementById("d1_9x1p").checked;
+        var d1_9x1_q = document.getElementById("d1_9x1q").checked;
+        var d1_9x1_r = document.getElementById("d1_9x1r").checked;
+        var d1_9x1_s = document.getElementById("d1_9x1s").checked;
+        var d1_9x1_t = document.getElementById("d1_9x1t").checked;
+        var d1_9x1_u = document.getElementById("d1_9x1u").checked;
+
+        // placeholder values
+        var d1_9x1_v1, d1_9x1_v2, d1_9x1_v3, d1_9x1_v4, d1_9x1_v5, d1_9x1_v6, d1_9x1_v7, d1_9x1_v8;
+        if (d1_9x1_a) {d1_9x1_v1 = document.getElementById("d1_9x1a").value;}
+        if (d1_9x1_b) {d1_9x1_v1 = document.getElementById("d1_9x1b").value;}
+        if (d1_9x1_c) {d1_9x1_v2 = document.getElementById("d1_9x1c").value;}
+        if (d1_9x1_d) {d1_9x1_v2 = document.getElementById("d1_9x1d").value;}
+        if (d1_9x1_e) {d1_9x1_v3 = document.getElementById("d1_9x1e").value;}
+        if (d1_9x1_f) {d1_9x1_v3 = document.getElementById("d1_9x1f").value;}
+        if (d1_9x1_g) {d1_9x1_v4 = document.getElementById("d1_9x1g").value;}
+        if (d1_9x1_h) {d1_9x1_v4 = document.getElementById("d1_9x1h").value;}
+        if (d1_9x1_i) {d1_9x1_v5 = document.getElementById("d1_9x1i").value;}
+        if (d1_9x1_j) {d1_9x1_v5 = document.getElementById("d1_9x1j").value;}
+        if (d1_9x1_k) {d1_9x1_v5 = document.getElementById("d1_9x1k").value;}
+        if (d1_9x1_l) {d1_9x1_v5 = document.getElementById("d1_9x1l").value;}
+        if (d1_9x1_m) {d1_9x1_v6 = document.getElementById("d1_9x1m").value;}
+        if (d1_9x1_n) {d1_9x1_v6 = document.getElementById("d1_9x1n").value;}
+        if (d1_9x1_o) {d1_9x1_v6 = document.getElementById("d1_9x1o").value;}
+        if (d1_9x1_p) {d1_9x1_v7 = document.getElementById("d1_9x1p").value;}
+        if (d1_9x1_q) {d1_9x1_v7 = document.getElementById("d1_9x1q").value;}
+        if (d1_9x1_r) {d1_9x1_v7 = document.getElementById("d1_9x1r").value;}
+        if (d1_9x1_s) {d1_9x1_v7 = document.getElementById("d1_9x1s").value;}
+        if (d1_9x1_t) {d1_9x1_v8 = document.getElementById("d1_9x1t").value;}
+        if (d1_9x1_u) {d1_9x1_v8 = document.getElementById("d1_9x1u").value;}
+
+        // determine if answers are correct
+        var d1_9x1_user = [d1_9x1_v1, d1_9x1_v2, d1_9x1_v3, d1_9x1_v4, d1_9x1_v5, d1_9x1_v6, d1_9x1_v7, d1_9x1_v8];
+        var d1_9x1_key = ["Yes", "F", "Yes", "Yes", "Distributive", "Contradiction", "Converse", "F"];
+        var d1_9x1_bool = [false, false, false, false, false, false, false, false];
+        for (i = 0; i < d1_9x1_key.length; i++) {
+            if (d1_9x1_user[i] == d1_9x1_key[i]) {d1_9x1_bool[i] = true;}
+        }
+
+        document.getElementById("d1_9x1_y1").innerHTML = d1_9x1_v1;
+        document.getElementById("d1_9x1_y2").innerHTML = d1_9x1_v2;
+        document.getElementById("d1_9x1_y3").innerHTML = d1_9x1_v3;
+        document.getElementById("d1_9x1_y4").innerHTML = d1_9x1_v4;
+        document.getElementById("d1_9x1_y5").innerHTML = d1_9x1_v5;
+        document.getElementById("d1_9x1_y6").innerHTML = d1_9x1_v6;
+        document.getElementById("d1_9x1_y7").innerHTML = d1_9x1_v7;
+        document.getElementById("d1_9x1_y8").innerHTML = d1_9x1_v8;
+
+        // if incorrect
+        if (!d1_9x1_bool[0]) {document.getElementById("d1_9x1_r1").style.backgroundColor = "#B5534F"; document.getElementById("d1_9x1_r1").innerHTML = "incorrect";}
+        if (!d1_9x1_bool[1]) {document.getElementById("d1_9x1_r2").style.backgroundColor = "#B5534F"; document.getElementById("d1_9x1_r2").innerHTML = "incorrect";}
+        if (!d1_9x1_bool[2]) {document.getElementById("d1_9x1_r3").style.backgroundColor = "#B5534F"; document.getElementById("d1_9x1_r3").innerHTML = "incorrect";}
+        if (!d1_9x1_bool[3]) {document.getElementById("d1_9x1_r4").style.backgroundColor = "#B5534F"; document.getElementById("d1_9x1_r4").innerHTML = "incorrect";}
+        if (!d1_9x1_bool[4]) {document.getElementById("d1_9x1_r5").style.backgroundColor = "#B5534F"; document.getElementById("d1_9x1_r5").innerHTML = "incorrect";}
+        if (!d1_9x1_bool[5]) {document.getElementById("d1_9x1_r6").style.backgroundColor = "#B5534F"; document.getElementById("d1_9x1_r6").innerHTML = "incorrect";}
+        if (!d1_9x1_bool[6]) {document.getElementById("d1_9x1_r7").style.backgroundColor = "#B5534F"; document.getElementById("d1_9x1_r7").innerHTML = "incorrect";}
+        if (!d1_9x1_bool[7]) {document.getElementById("d1_9x1_r8").style.backgroundColor = "#B5534F"; document.getElementById("d1_9x1_r8").innerHTML = "incorrect";}
+
+        // if correct
+        if (d1_9x1_bool[0]) {document.getElementById("d1_9x1_r1").style.backgroundColor = "#739E5B"; document.getElementById("d1_9x1_r1").innerHTML = "correct";}
+        if (d1_9x1_bool[1]) {document.getElementById("d1_9x1_r2").style.backgroundColor = "#739E5B"; document.getElementById("d1_9x1_r2").innerHTML = "correct";}
+        if (d1_9x1_bool[2]) {document.getElementById("d1_9x1_r3").style.backgroundColor = "#739E5B"; document.getElementById("d1_9x1_r3").innerHTML = "correct";}
+        if (d1_9x1_bool[3]) {document.getElementById("d1_9x1_r4").style.backgroundColor = "#739E5B"; document.getElementById("d1_9x1_r4").innerHTML = "correct";}
+        if (d1_9x1_bool[4]) {document.getElementById("d1_9x1_r5").style.backgroundColor = "#739E5B"; document.getElementById("d1_9x1_r5").innerHTML = "correct";}
+        if (d1_9x1_bool[5]) {document.getElementById("d1_9x1_r6").style.backgroundColor = "#739E5B"; document.getElementById("d1_9x1_r6").innerHTML = "correct";}
+        if (d1_9x1_bool[6]) {document.getElementById("d1_9x1_r7").style.backgroundColor = "#739E5B"; document.getElementById("d1_9x1_r7").innerHTML = "correct";}
+        if (d1_9x1_bool[7]) {document.getElementById("d1_9x1_r8").style.backgroundColor = "#739E5B"; document.getElementById("d1_9x1_r8").innerHTML = "correct";}
+    }
     // 2.1
     // 2.2
     // 2.3
